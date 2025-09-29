52d89cc76358276dc33d6da291c65d98983016c7
rohit175041 <rohit175041@hmail.com>
2025-09-29 19:15:33 +0530

    remove name

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
         alt="logo"
         style={{ height: "60px" }}
       />
-      <h1 style={{ margin: "0.5rem 0", fontSize: "2rem" }}>WaveRTC</h1>
+      {/* <h1 style={{ margin: "0.5rem 0", fontSize: "2rem" }}>WaveRTC</h1> */}
       <p style={{ margin: 0, fontSize: "1rem", color: "#ddd" }}>
         Peer-to-peer video chat
       </p>
