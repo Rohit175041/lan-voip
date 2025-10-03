79fdefa93f24f2dc162a110c2613a3b190e43125
rohit175041 <rohit175041@hmail.com>
2025-10-03 12:01:11 +0530

    improve connected button

--- src/components/StatusIndicator.js
+++ src/components/StatusIndicator.js
@@ -1,24 +1,5 @@
 import "./StatusIndicator.css";
-import { FaCheckCircle, FaClock, FaTimesCircle } from "react-icons/fa";
 
 export default function StatusIndicator({ status }) {
-  return (
-    <div className="status-indicator">
-      {status === "connected" && (
-        <span className="status-pill glass connected">
-          <FaCheckCircle className="status-icon" /> Connected
-        </span>
-      )}
-      {status === "waiting" && (
-        <span className="status-pill glass waiting">
-          <FaClock className="status-icon" /> Waiting...
-        </span>
-      )}
-      {status === "disconnected" && (
-        <span className="status-pill glass disconnected">
-          <FaTimesCircle className="status-icon" /> Disconnected
-        </span>
-      )}
-    </div>
-  );
+  return <div className={`status-ball ${status}`} />;
 }
